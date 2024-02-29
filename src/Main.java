c5488b3ca464ccca6a58c3e57fbdeb8664477f88
sonny <dhsmf4245@gmail.com>
2024-02-29 10:27:05 +0900

    algorithm dp

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 import java.util.*;
 public class Main {
     public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
 
+        for(int i = 0; i < n; i++){
+            arr[i] =sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int num = (int)Math.round(n*15/100);
+        double sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+        }
+        int answer = (int)Math.round(sum/(n-2*num));
+        System.out.print(answer);
     }
 
 }
