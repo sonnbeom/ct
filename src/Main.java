1638709b62a302634737a3cf412de8e6e2739cbd
sonny <dhsmf4245@gmail.com>
2024-03-05 12:22:30 +0900

    algorithm implementation

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,37 @@ public class Main {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Stack<Integer> stack = new Stack<>();
+        int sum = 0;
+        int centerCount = 0;
+        int center = (n+1) / 2;
+        int[] arr = new int[n];
         for(int i = 0; i < n; i++){
             int input = sc.nextInt();
-            if(input != 0){
-                stack.push(input);
-            }
-            else{
-                stack.pop();
+            arr[i] = input;
+            sum += input;
+            if(i == center){
+                centerCount = input;
             }
         }
-        int sum = 0;
-        for(int v : stack){
-            sum += v;
+        int min = 0;
+        int max = 0;
+        for(int i = 0; i <n-1; i++){
+            min = Math.min(arr[i], arr[i+1]);
+            max = Math.max(arr[i], arr[i+1]);
         }
-        System.out.print(sum);
+        // 1. 산술평균
+        System.out.println(sum/n);
+        // 2. 중앙값
+        System.out.println(centerCount);
+        // 3. 최빈값
+
+        // 4. 범위
+        System.out.println(max - min);
+
     }
 }
+/*
+* 5 3
+* 7 4
+* 9 5
+* */
