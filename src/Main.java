2f5b81ae3bbb77fdc0a1ea87a55e8bf0a8c4f639
sonny <dhsmf4245@gmail.com>
2024-03-07 13:20:36 +0900

    algorithm implementation

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,16 @@ public class Main {
     public static void main(String[]args) {
        Scanner sc = new Scanner(System.in);
        int n = sc.nextInt();
-       int[] arr = new int[n+1];
+       int count = 0;
+       int plus = 1;
+       int num = 10;
        for(int i = 1; i <= n; i++){
-           arr[i] = sc.nextInt();
-       }
-       int k = sc.nextInt();
-       for(int i = 0; i < k; i++){
-           int gender = sc.nextInt();
-           int num = sc.nextInt();
-           if(gender == 1){
-               for(int j = num; j <= n; j += num){
-                   arr[j] = arr[j] == 1? 0 : 1;
-               }
+           if(i % num == 0){
+               plus++;
+               num *= 10;
            }
-           else{
-               arr[num] = arr[num] == 0? 1 : 0;
-               for(int j = 1; j < n/2; j++){
-                   if(num - j < 1 || num + j >n){
-                       break;
-                   }
-                   if(arr[num-j] == arr[num+j]){
-                       arr[num - j] = arr[num - j] == 1? 0 : 1;
-                       arr[num + j] = arr[num + j] == 1? 0 : 1;
-                   }
-                   else{
-                       break;
-                   }
-               }
-           }
+           count += plus;
        }
-       for(int i = 1; i <= n; i++){
-           System.out.print(arr[i]+" ");
-           if(i % 20 ==0){
-               System.out.println();
-           }
-       }
+       System.out.print(count);
     }
 }
