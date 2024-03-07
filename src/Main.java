a6ba2787f68d4d2de11c2034935457c3dad084bf
sonny <dhsmf4245@gmail.com>
2024-03-07 11:50:08 +0900

    algorithm implementation

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,43 @@
 import java.util.*;
 public class Main {
-    static int N;
-    static int K;
-    static boolean[] check;
 
     public static void main(String[]args) {
        Scanner sc = new Scanner(System.in);
-       N = sc.nextInt();
-       K = sc.nextInt();
-       check = new boolean[N+1];
-       primeNumber();
-
-    }
-    static void primeNumber(){
-        for(int i = 2; i <= N; i++){
-           if(!check[i]){
-               for(int j = i; j <= N; j+=i){
-                   check[j] = true;
-                   K--;
-                   if(K == 0){
-                       System.out.print(j);
-                       return;
+       int n = sc.nextInt();
+       int[] arr = new int[n+1];
+       for(int i = 1; i <= n; i++){
+           arr[i] = sc.nextInt();
+       }
+       int k = sc.nextInt();
+       for(int i = 0; i < k; i++){
+           int gender = sc.nextInt();
+           int num = sc.nextInt();
+           if(gender == 1){
+               for(int j = num; j <= n; j += num){
+                   arr[j] = arr[j] == 1? 0 : 1;
+               }
+           }
+           else{
+               arr[num] = arr[num] == 0? 1 : 0;
+               for(int j = 1; j < n/2; j++){
+                   if(num - j < 1 || num + j >n){
+                       break;
+                   }
+                   if(arr[num-j] == arr[num+j]){
+                       arr[num - j] = arr[num - j] == 1? 0 : 1;
+                       arr[num + j] = arr[num + j] == 1? 0 : 1;
+                   }
+                   else{
+                       break;
                    }
                }
            }
-        }
+       }
+       for(int i = 1; i <= n; i++){
+           System.out.print(arr[i]+" ");
+           if(i % 20 ==0){
+               System.out.println();
+           }
+       }
     }
 }
---
+++ src/codingtest/Silver4_1244.java
@@ -0,0 +1,45 @@
+package codingtest;
+
+import java.util.Scanner;
+
+public class Silver4_1244 {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n+1];
+        for(int i = 1; i <= n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        for(int i = 0; i < k; i++){
+            int gender = sc.nextInt();
+            int num = sc.nextInt();
+            if(gender == 1){
+                for(int j = num; j <= n; j += num){
+                    arr[j] = arr[j] == 1? 0 : 1;
+                }
+            }
+            else{
+                arr[num] = arr[num] == 0? 1 : 0;
+                for(int j = 1; j < n/2; j++){
+                    if(num - j < 1 || num + j >n){
+                        break;
+                    }
+                    if(arr[num-j] == arr[num+j]){
+                        arr[num - j] = arr[num - j] == 1? 0 : 1;
+                        arr[num + j] = arr[num + j] == 1? 0 : 1;
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        }
+        for(int i = 1; i <= n; i++){
+            System.out.print(arr[i]+" ");
+            if(i % 20 ==0){
+                System.out.println();
+            }
+        }
+    }
+}
