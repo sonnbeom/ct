181712b2ab4aebc7af101f3e726af42ae6697a53
SON\dhsmf <dhsmf4245@gmail.com>
2024-03-13 12:34:09 +0900

    a

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,6 @@ import java.util.*;
 public class Main {
 
     public static void main(String[]args) {
-       Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt();
-       int count = 0;
-       int plus = 1;
-       int num = 10;
-       for(int i = 1; i <= n; i++){
-           if(i % num == 0){
-               plus++;
-               num *= 10;
-           }
-           count += plus;
-       }
-       System.out.print(count);
+
     }
 }
