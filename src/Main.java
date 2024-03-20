cae7d9150fa340a0b1b7294e2a15671f7f327f68
SON\dhsmf <dhsmf4245@gmail.com>
2024-03-20 14:03:56 +0900

    1135

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,6 @@ import java.util.*;
 public class Main {
 
     public static void main(String[]args) {
-        int i = 3;
-        int t = 2;
-        String st = "azzz";
-        i = 4;
-        i++;
-        System.out.println(i);
 
     }
 }
