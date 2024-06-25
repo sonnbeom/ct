706cf2349a6fef3bfa9d982346d0d09568aebfb0
sonny <dhsmf4245@gmail.com>
2024-06-25 20:11:39 +0900

    알고리즘 sw 풀이 d2, d1, d1

---
+++ src/algorithm/D2_2개의_숫자열.java
@@ -0,0 +1,47 @@
+package algorithm;
+
+import java.util.*;
+public class D2_2개의_숫자열 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 1; i <= t; i++){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[] arrFi = new int[n];
+            int[] arrSe = new int[n];
+
+            for(int j = 0; j < n; j++){
+                arrFi[j] = sc.nextInt();
+            }
+
+            for (int j = 0; j < m; j++) {
+                arrSe[j] = sc.nextInt();
+            }
+            int diff = 0;
+            int leng = 0;
+            if (n > m){
+                diff = n - m;
+                leng = m;
+            }else {
+                diff = m - n;
+                leng = n;
+            }
+            int max = 0;
+            for (int j = 0; j <= diff; j++) {
+                int temp = 0;
+                for (int k = 0; k <= leng; k++) {
+                    temp += arrFi[k] + arrSe[k];
+                }
+                if (max < temp){
+                    max = temp;
+                }
+            }
+            System.out.println("#"+i+" "+max);
+        }
+    }
+}
+/*
+*
+*
+* */
--- src/algorithm/S_1206.java
+++ src/algorithm/S_1206.java
@@ -5,5 +5,21 @@ import java.util.Scanner;
 public class S_1206 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int temp = N;
+        int length = 0;
+        while (temp > 0){
+            temp /= 10;
+            length++;
+        }
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += N % 10;
+            int t = N /10;
+            N = t;
+        }
+        System.out.println(sum);
+
+
     }
 }
