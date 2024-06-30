35ee9f66d317dbc936d5c72853d5a22d5e962097
sonny <dhsmf4245@gmail.com>
2024-06-30 09:46:45 +0900

    d3 2342

---
+++ src/algorithm/D3_2342.java
@@ -0,0 +1,25 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class D3_2342 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int temp = N;
+        int length = 0;
+        while (temp > 0){
+            temp /= 10;
+            length++;
+        }
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += N % 10;
+            int t = N /10;
+            N = t;
+        }
+        System.out.println(sum);
+
+
+    }
+}
